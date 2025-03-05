002e2000fd7be92198c11c6f8b749e0f94c66fc4
joelsenberg22 <bperezs14@miumg.edu.gt>
2025-03-05 00:28:52 -0600

    se sube archivo ejemplos de clase 5

---
+++ Clase 5 Estructuras de control.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+main(){
+	char respuesta;
+	do{
+		cout<<"desea ingresar otro valor (s/n)";
+		cin>>respuesta;
+			}while(respuesta=='s' || respuesta=='S' );	
+}
+
+
+
+/*
+	int i = 0;
+	while(i<5){
+		cout<<i<<endl;   //error de novato, ciclo infinito
+	}
+*/
+
+	/*
+	int i = 0;
+	while(i<5){
+	
+		i++;
+			cout<<i<<endl; 
+	*/
+/*
+		int i = 0;
+		do{
+				cout<<i<<endl; 
+					i++;
+	}while(i<5);{
+	
+	
+		
+}
+*/
